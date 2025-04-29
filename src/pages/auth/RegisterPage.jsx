9f84776db84d3f84a7fca2bf358806ca251abfbb
Ashim Nepal <ashimnepal90@gmail.com>
2025-04-29 16:45:20 -0400

    Register Page Amemded

--- src/pages/auth/RegisterPage.jsx
+++ src/pages/auth/RegisterPage.jsx
@@ -1,11 +1,107 @@
 import React from "react";
+import { useState } from "react";
 
 
-function RegisterPage(){
-    return(
+function RegisterPage() {
+    const[data, setData] = useState();
+    return (
         <React.Fragment>
-       
-            <h1>Register PAge</h1>
+
+            <section className="bg-gray-50 dark:bg-gray-900">
+                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto min-h-screen">
+                    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-xl xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                Create an account
+                            </h1>
+
+                            <form className="space-y-4 md:space-y-6" action="#">
+                                <div>
+                                    <label htmlFor="fullName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Full Name</label>
+                                    <input type="text" name="fullName" id="fullName"
+                                    onChange={(e) => {
+                                        const {value, name, type} = e.target;
+                                        console.log(value, name, type);
+                                    }} 
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-teal-600 focus:border-teal-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="Full Name" required=""
+                                    
+                                    />
+                                    
+                                       
+                                    
+                                </div>
+                                
+                                <div>
+                                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                                    <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-teal-600 focus:border-teal-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email" required="" />
+                                </div>
+
+                                <div className="grid grid-cols-6 md:grid-cols-2 gap-4">
+                                    <div>
+                                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                        <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-teal-600 focus:border-teal-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
+                                        <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-teal-600 focus:border-teal-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                                    </div>
+                                </div>
+                                
+                                <div>
+                                    <label htmlFor="role" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Role</label>
+                                    <select name="role" id="role" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-teal-600 focus:border-teal-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                        <option value="">--Select Role--</option>
+                                        <option value="customer">Buyer</option>
+                                        <option value="seller">Seller</option>
+                                    </select>
+                                </div>
+                                    <div>
+                                        <label htmlFor="gender" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Gender</label>
+                                        <div className="flex items-center gap-2 text-white">
+                                        <input type="radio" name="gender" id="gender" value={"male"} />
+                                        <label htmlFor="male">Male</label>
+                                        <input type="radio" name="gender" id="gender" value={"female"} />
+                                        <label htmlFor="female">Female</label>
+                                        <input type="radio" name="gender" id="gender" value={"others"} />
+                                        <label htmlFor="others">Others</label>
+                                    </div>
+                                    </div>
+
+
+
+
+                                <div>
+                                    <label htmlFor="tel" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Telephone</label>
+                                    <input type="tel" name="tel" id="tel" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-teal-600 focus:border-teal-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Telephone" required="" />
+                                </div>
+                                
+                                <textarea name="address" id="address" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-teal-600 focus:border-teal-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Address" required="" rows={5} style={{resize:"none"}} ></textarea>
+
+                                <div>
+                                    <label htmlFor="Image" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Upload Profile Picture</label>
+                                    <input type="file" name="Image" id="Image" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-teal-600 focus:border-teal-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                                </div>
+
+                                
+
+                                <div className="flex items-start">
+                                    <div className="flex items-center h-5">
+                                        <input id="terms" aria-describedby="terms" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-teal-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-teal-600 dark:ring-offset-gray-800" required="" />
+                                    </div>
+                                    <div className="ml-3 text-sm">
+                                        <label htmlFor="terms" className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-teal-600 hover:underline dark:text-teal-500" href="#">Terms and Conditions</a></label>
+                                    </div>
+                                </div>
+                                <button type="submit" className="w-full text-white bg-teal-600 hover:bg-teal-700 focus:ring-4 focus:outline-none focus:ring-teal-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-teal-600 dark:hover:bg-teal-700 dark:focus:ring-teal-800">Create an account</button>
+                                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                    Already have an account? <a href="#" className="font-medium text-teal-600 hover:underline dark:text-teal-500">Login here</a>
+                                </p>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </React.Fragment>
     )
 }
